da0f87e6fba4279f84dab120cd09f12f64a66d44
Abdunur <amangeldievabdunur@gmail.com>
2025-04-10 17:44:08 +0600

    modified MyArrayList

--- Modules/MyArrayList.java
+++ Modules/MyArrayList.java
@@ -1,5 +1,4 @@
 package Modules;
-import Modules.MyList;
 
 public class MyArrayList<T> implements MyList<T> {
     private Object[] elements;
@@ -20,31 +19,38 @@ public class MyArrayList<T> implements MyList<T> {
     private void increaseCapacity() {
         Object[] newElements = new Object[elements.length * 2];
         for (int i = 0; i < elements.length; i++) {
-            newElements[i] = elements[i]; //upcasting
+            newElements[i] = elements[i];
         }
         elements = newElements;
     }
 
     public T get(int index) {
         checkIndex(index);
-        return (T) elements[index]; //downcasting
+        return (T) elements[index];
     }
 
-    public void remove(int index){
+    public void remove(int index) {
         checkIndex(index);
         for (int i = index; i < length - 1; i++) {
-            elements[i] = elements[i+1];
+            elements[i] = elements[i + 1];
         }
+        elements[--length] = null;
     }
 
-    private void checkIndex(int index){
+    public void clear() {
+        for (int i = 0; i < length; i++) {
+            elements[i] = null;
+        }
+        length = 0;
+    }
+
+    private void checkIndex(int index) {
         if (index < 0 || index >= length) {
-            throw new IndexOutOfBoundsException("Index: " + index + " not found");
+            throw new IndexOutOfBoundsException("Index: " + index);
         }
     }
 
     public int size() {
         return length;
     }
-
 }
